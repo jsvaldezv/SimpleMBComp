6c04a3886635ed1540050fcfba89502eb81fbfd9
Jesús Valdez <jsvaldezv@yahoo.com>
2022-02-02 17:17:25 -0600

    Clean comments

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,31 +1,16 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-//==============================================================================
 SimpleMBCompAudioProcessorEditor::SimpleMBCompAudioProcessorEditor (SimpleMBCompAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
     setSize (400, 300);
 }
 
-SimpleMBCompAudioProcessorEditor::~SimpleMBCompAudioProcessorEditor()
-{
-}
+SimpleMBCompAudioProcessorEditor::~SimpleMBCompAudioProcessorEditor(){}
 
-//==============================================================================
 void SimpleMBCompAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setColour (juce::Colours::white);
@@ -33,8 +18,4 @@ void SimpleMBCompAudioProcessorEditor::paint (juce::Graphics& g)
     g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
-void SimpleMBCompAudioProcessorEditor::resized()
-{
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
-}
+void SimpleMBCompAudioProcessorEditor::resized(){}
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,15 +1,6 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-//==============================================================================
 SimpleMBCompAudioProcessor::SimpleMBCompAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
      : AudioProcessor (BusesProperties()
@@ -24,11 +15,8 @@ SimpleMBCompAudioProcessor::SimpleMBCompAudioProcessor()
 {
 }
 
-SimpleMBCompAudioProcessor::~SimpleMBCompAudioProcessor()
-{
-}
+SimpleMBCompAudioProcessor::~SimpleMBCompAudioProcessor(){}
 
-//==============================================================================
 const juce::String SimpleMBCompAudioProcessor::getName() const
 {
     return JucePlugin_Name;
@@ -68,8 +56,7 @@ double SimpleMBCompAudioProcessor::getTailLengthSeconds() const
 
 int SimpleMBCompAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1;
 }
 
 int SimpleMBCompAudioProcessor::getCurrentProgram()
@@ -77,31 +64,18 @@ int SimpleMBCompAudioProcessor::getCurrentProgram()
     return 0;
 }
 
-void SimpleMBCompAudioProcessor::setCurrentProgram (int index)
-{
-}
+void SimpleMBCompAudioProcessor::setCurrentProgram (int index){}
 
 const juce::String SimpleMBCompAudioProcessor::getProgramName (int index)
 {
     return {};
 }
 
-void SimpleMBCompAudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
-}
+void SimpleMBCompAudioProcessor::changeProgramName (int index, const juce::String& newName){}
 
-//==============================================================================
-void SimpleMBCompAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
-{
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
-}
+void SimpleMBCompAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock){}
 
-void SimpleMBCompAudioProcessor::releaseResources()
-{
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
-}
+void SimpleMBCompAudioProcessor::releaseResources(){}
 
 #ifndef JucePlugin_PreferredChannelConfigurations
 bool SimpleMBCompAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
@@ -110,20 +84,13 @@ bool SimpleMBCompAudioProcessor::isBusesLayoutSupported (const BusesLayout& layo
     juce::ignoreUnused (layouts);
     return true;
   #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
      && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
-
-    // This checks if the input layout matches the output layout
    #if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
    #endif
-
     return true;
   #endif
 }
@@ -135,33 +102,18 @@ void SimpleMBCompAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
         auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
     }
 }
 
-//==============================================================================
 bool SimpleMBCompAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+    return true;
 }
 
 juce::AudioProcessorEditor* SimpleMBCompAudioProcessor::createEditor()
@@ -169,22 +121,11 @@ juce::AudioProcessorEditor* SimpleMBCompAudioProcessor::createEditor()
     return new SimpleMBCompAudioProcessorEditor (*this);
 }
 
-//==============================================================================
-void SimpleMBCompAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
-{
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
-}
 
-void SimpleMBCompAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
-{
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
-}
+void SimpleMBCompAudioProcessor::getStateInformation (juce::MemoryBlock& destData){}
 
-//==============================================================================
-// This creates new instances of the plugin..
+void SimpleMBCompAudioProcessor::setStateInformation (const void* data, int sizeInBytes){}
+
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new SimpleMBCompAudioProcessor();
